a8f53f6f677bd50a3616f51e50dea6b5833b062e
Leonardo Negrão <leonardolnegrao@gmail.com>
2020-03-25 17:30:09 -0300

    montagem da page quemSomos

--- src/pages/quemSomos.js
+++ src/pages/quemSomos.js
@@ -2,10 +2,61 @@ import React from "react"
 
 import Layout from "../components/layout"
 
+import "../styles/quemSomos.css"
+
+import avatar from "../images/53981024.png"
+
 const QuemSomos = () => {
   return (
     <Layout>
-      <h2>QuemSomos</h2>
+      <div className="quem-somos">
+        <h2 className="title">Quem Somos</h2>
+        <p id="descricao">
+          Somos três estudantes de Engenharia de Software da UniFil, atualmente
+          no 3º Período.
+        </p>
+        <div>
+          <div className="perfil-wrapper">
+            <div>
+              <img src={avatar} height="150"></img>
+              <h3>Leonardo Negrão</h3>
+            </div>
+            <p>
+              Estudante de Engenharia de Software na UniFil. Estudando React,
+              NodeJS, entre outras tecnologias web, como também Java.
+            </p>
+            <a href="https://github.com/leonardonegrao" target="_blank">
+              GitHub
+            </a>
+          </div>
+          <div className="perfil-wrapper">
+            <div>
+              <img src={avatar} height="150"></img>
+              <h3>Matheus Pedrero</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse commodo orci et risus rutrum, nec tincidunt neque
+              sollicitudin. Ut sodales molestie aliquet. Etiam sodales finibus
+              efficitur. Donec id augue tincidunt, euismod ipsum feugiat,
+              sodales ipsum.
+            </p>
+          </div>
+          <div className="perfil-wrapper">
+            <div>
+              <img src={avatar} height="150"></img>
+              <h3>Daniel Pereira</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse commodo orci et risus rutrum, nec tincidunt neque
+              sollicitudin. Ut sodales molestie aliquet. Etiam sodales finibus
+              efficitur. Donec id augue tincidunt, euismod ipsum feugiat,
+              sodales ipsum.
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
