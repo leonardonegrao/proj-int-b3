8ae829289462feea7c02a000e7b173c72957f30d
Leonardo Negrão <leonardolnegrao@gmail.com>
2020-04-27 23:21:31 -0300

    integração com disqus

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { DiscussionEmbed } from "disqus-react"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
@@ -12,7 +13,8 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       subtitle
-      date
+      date(formatString: "LLLL", locale: "pt-br")
+      updatedAt(formatString: "LLLL", locale: "pt-br")
       body {
         json
       }
@@ -30,6 +32,15 @@ const Blog = props => {
       },
     },
   }
+
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: {
+      identifier: props.data.contentfulBlogPost.slug,
+      title: props.data.contentfulBlogPost.title,
+    },
+  }
+
   return (
     <Layout>
       <Head title={props.data.contentfulBlogPost.title} />
@@ -39,12 +50,22 @@ const Blog = props => {
             <article className={blogPostStyles.post}>
               <h1>{props.data.contentfulBlogPost.title}</h1>
               <h2>{props.data.contentfulBlogPost.subtitle}</h2>
-              <h3>{props.data.contentfulBlogPost.date}</h3>
+              <sub>{props.data.contentfulBlogPost.date}</sub>
+              <div className={blogPostStyles.postBody}>
+                {documentToReactComponents(
+                  props.data.contentfulBlogPost.body.json,
+                  options
+                )}
+              </div>
 
-              {documentToReactComponents(
-                props.data.contentfulBlogPost.body.json,
-                options
-              )}
+              <sub>Criado {props.data.contentfulBlogPost.date}</sub>
+              <br></br>
+              <sub>
+                Última vez atualizado {props.data.contentfulBlogPost.updatedAt}
+              </sub>
+              <div className={blogPostStyles.disqus}>
+                <DiscussionEmbed {...disqusConfig} />
+              </div>
             </article>
           </div>
         </div>
